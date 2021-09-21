c4298f714eab63446ed89a2bc9e529fa78bde154
Erecik <eret112@gmail.com>
2021-09-22 00:53:07 +0200

    Payment app added

---
+++ payment/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+def PaypalToken(client_ID, client_Secret):
+
+    url = "https://api.sandbox.paypal.com/v1/oauth2/token"
+    data = {
+                "client_id":client_ID,
+                "client_secret":client_Secret,
+                "grant_type":"client_credentials"
+            }
+    headers = {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Authorization": "Basic {0}".format(base64.b64encode((client_id + ":" + client_secret).encode()).decode())
+            }
+
+    token = requests.post(url, data, headers=headers)
+    return token
